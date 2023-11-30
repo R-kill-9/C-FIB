46a58ff285fab09e86b18ba6c39904e8c8c327a4
kill-9 <ricardmeam@gmail.com>
2023-11-30 07:49:28 +0100

    blockchain ok

--- Practica3/blockchain.py
+++ Practica3/blockchain.py
@@ -59,6 +59,7 @@ class block:
         while not next_block.verify_block():
             next_block.seed = random.randrange(0,2**256)
             next_block.block_hash = next_block.create_hash()
+        print("verified")
         return next_block
 
 
@@ -125,7 +126,7 @@ class block_chain:
     
     # verifica que todos los bloques sean validos y que el siguiente bloque es correcto
     # recorre la lista desde el final empezando por la penultima posicion
-        i = 0
+        i = 1
         for block in self.list_of_blocks[1:]:
             actual_previous = self.list_of_blocks[i].previous_block_hash
             previous = self.list_of_blocks[i-1].block_hash
